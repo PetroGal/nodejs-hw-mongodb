d27bf793b6d057b694ff221a4728ea3a88991ec6
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-08-31 19:34:46 +0300

    completed home work

--- server.js
+++ server.js
@@ -28,8 +28,14 @@ export const setupServer = () => {
   });
 
   app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = res.params;
+    const { contactId } = req.params;
     const data = await contactServices.getContactById(contactId);
+
+    if (!data) {
+      return res.status(404).json({
+        message: 'Contact not found',
+      });
+    }
     res.json({
       status: 200,
       message: 'Successfully found contact with id {contactId}!',
